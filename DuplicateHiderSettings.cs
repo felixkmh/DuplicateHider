70e5d41bbdd079988be06b172fdcf08d688e2062
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2020-10-20 20:12:24 +0200

    added new option to load function

--- DuplicateHiderSettings.cs
+++ DuplicateHiderSettings.cs
@@ -69,6 +69,7 @@ namespace DuplicateHider
                 ExcludeCategories = savedSettings.ExcludeCategories;
                 ExcludeSources = savedSettings.ExcludeSources;
                 IgnoredGames = savedSettings.IgnoredGames;
+                ShowOtherCopiesInGameMenu = savedSettings.ShowOtherCopiesInGameMenu;
             }
         }
 
