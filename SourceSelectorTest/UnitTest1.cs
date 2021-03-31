14b2405f0c80e65c04262db06d870009d8765ce5
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2021-03-31 19:40:29 +0200

    test

--- SourceSelectorTest/UnitTest1.cs
+++ SourceSelectorTest/UnitTest1.cs
@@ -13,6 +13,9 @@ namespace SourceSelectorTest
             var names = SourceSelector.GetResourceNames();
             var ubisoft = SourceSelector.GetResourceIconUri("ubisoft connect");
             var steam = SourceSelector.GetResourceIconUri("steam");
+            var asm = System.Reflection.Assembly.GetAssembly(typeof(DuplicateHider.DuplicateHider)).GetName();
+            var codebase = asm.EscapedCodeBase;
+            var uri = new Uri($"pack://application:,,,/{asm.Name};component/icons/undefined.ico");
         }
     }
 }
