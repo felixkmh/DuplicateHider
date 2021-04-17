69880c1940bff08354770e24cf919ffbaacf4e1a
Felix Holler <24227002+felixkmh@users.noreply.github.com>
2021-04-17 19:39:42 +0200

    fixed right drop shadow

---
+++ source/Models/DuplicateEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateHider.Models
+{
+    public struct DuplicateEntry
+    {
+        public Guid gameId;
+        public bool isEdition;
+        public string editionTag;
+
+        public override int GetHashCode()
+        {
+            return gameId.GetHashCode();
+        }
+    }
+}
